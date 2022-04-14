ef655aedae89d1a8f251612a06d3fdd22149e600
Daniel Fernandes <daniel.fernandes@sbcredito.com.br>
2022-04-14 19:03:51 -0300

    Add supplier controller

--- src/Services/Supplier/Controllers/SupplierController.cs
+++ src/Services/Supplier/Controllers/SupplierController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fornecedor.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class FornecedorController : ControllerBase
@@ -12,4 +13,59 @@ public class FornecedorController : ControllerBase
     {
         _logger = logger;
     }
+
+    /// <summary>
+    /// Create a new supplier.
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> Create()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Get all suppliers.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [HttpPost]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Get supplier by Id.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    [HttpPost]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Update supplier data.
+    /// </summary>
+    /// <returns></returns>
+    [HttpPut("{id}")]
+    [HttpPost]
+    public async Task<IActionResult> Update(Guid id, [FromBody] CustomerUpdateViewModel dto)
+    {
+        return Ok();
+    }
+    
+    /// <summary>
+    /// Delete supplier.
+    /// </summary>
+    /// <returns></returns>
+    [HttpPut("{id}")]
+    [HttpPost]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        return Ok();
+    }
 }
+
